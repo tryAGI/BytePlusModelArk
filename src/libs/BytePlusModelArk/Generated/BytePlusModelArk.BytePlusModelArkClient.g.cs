f009ea64293f5cb2d8c7bfdfbeb07f97d744e62c
Konstantin S. <havendv@gmail.com>
2026-05-05 20:26:59 +0400

    feat: Updated OpenAPI spec (#2)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/BytePlusModelArk/Generated/BytePlusModelArk.BytePlusModelArkClient.g.cs
+++ src/libs/BytePlusModelArk/Generated/BytePlusModelArk.BytePlusModelArkClient.g.cs
@@ -73,10 +73,10 @@ namespace BytePlusModelArk
         /// <param name="options">Client-wide request defaults such as headers, query parameters, retries, and timeout.</param>
         /// <param name="disposeHttpClient">Dispose the HttpClient when the instance is disposed. True by default.</param>
         public BytePlusModelArkClient(
-            global::System.Net.Http.HttpClient? httpClient = null,
-            global::System.Uri? baseUri = null,
-            global::System.Collections.Generic.List<global::BytePlusModelArk.EndPointAuthorization>? authorizations = null,
-            global::BytePlusModelArk.AutoSDKClientOptions? options = null,
+            global::System.Net.Http.HttpClient? httpClient,
+            global::System.Uri? baseUri,
+            global::System.Collections.Generic.List<global::BytePlusModelArk.EndPointAuthorization>? authorizations,
+            global::BytePlusModelArk.AutoSDKClientOptions? options,
             bool disposeHttpClient = true)
         {
 
