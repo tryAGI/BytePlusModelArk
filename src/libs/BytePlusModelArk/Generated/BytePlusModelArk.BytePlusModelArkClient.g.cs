118c3bc14e18dc3a5301103303d3188dcb1ca50f
Konstantin S. <havendv@gmail.com>
2026-08-14 19:45:03 +0400

    feat: Updated OpenAPI spec (#32)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/BytePlusModelArk/Generated/BytePlusModelArk.BytePlusModelArkClient.g.cs
+++ src/libs/BytePlusModelArk/Generated/BytePlusModelArk.BytePlusModelArkClient.g.cs
@@ -34,10 +34,17 @@ namespace BytePlusModelArk
 
         /// <inheritdoc/>
         public global::BytePlusModelArk.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::BytePlusModelArk.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::BytePlusModelArk.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
